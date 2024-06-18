dc8f9489a295b55f2d1e4f1786812172909b27a7
aryansharma <aryansharmag29@gmail.com>
2024-06-18 23:42:46 +0530

    updated

--- TAILWIND/tailwind.config.js
+++ TAILWIND/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./card.html"],
+  content: ["./card.html","./Assignment_6.html","./cup_card.html"],
   theme: {
     extend: {},
   },
