269cab836923baf6792e1f07efad23a5615e5f84
aryansharma <aryansharmag29@gmail.com>
2024-06-19 18:47:06 +0530

    portfolio_assign_8

--- TAILWIND/tailwind.config.js
+++ TAILWIND/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./card.html","./Assignment_6.html","./cup_card.html"],
+  content: ["./card.html","./Assignment_6.html","./cup_card.html","./portfolio.html"],
   theme: {
     extend: {},
   },
